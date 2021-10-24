3478dc0a207c8d9a139cb2af9ee5da5fe4452d49
Stephan Leibelt <stephan.leibelt@doctolib.com>
2021-10-24 12:37:45 +0200

    Update Ruby and gems

--- spec/requests/partners_spec.rb
+++ spec/requests/partners_spec.rb
@@ -5,9 +5,9 @@ require "swagger_helper"
 RSpec.describe PartnersController, type: :request do
   before do
     Partner.create(name: "foo", materials: ["parkett"], latitude: 15.44, longitude: 13.12, rating: 4.5)
-    end
+  end
 
-  it "returns" 
+  it "returns"
   run_test! do |response|
     partners = JSON.parse(response.body)
     expect(partners.size).to eq 1
